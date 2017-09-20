dd2a2a6d60d04daeac120a0e472cca384589a688
Timofey Rechkalov <trechkalov@yandex.ru>
2017-09-21 04:14:52 +0500

    Initial commit. Implemented gulp file. Made copy of 37 slides from 74 slides of original presentation.

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var fileInclude = require('gulp-file-include');
+var config = require('./config');
+
+var projectDir = 'app';
+var projectBase = 'app';
+var lang = config.get('lang');
+
+var htmls = [projectDir + '/index.html'];
+var partials = ['./translations/' + lang + '/**/*'];
+    
+gulp.task('copyPresentation', function() {
+    return gulp.src(projectDir + '/presentation' + '/**/*')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html', function() {
+    return gulp.src(htmls, {base : projectDir})
+    .pipe(fileInclude({
+      prefix: '@@',
+      basepath: './translations/' + lang + '/',
+      context: {
+          PAGE_TITLE: config.get('pageTitle')
+      }
+    }))
+//    .pipe(htmlmin({collapseWhitespace : true}))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('watch', function() {
+    
+    gulp.watch(htmls, ['html']);
+    gulp.watch(partials, ['html']);
+    
+});
+
+gulp.task('dev', ['copyPresentation', 'html','watch']);
